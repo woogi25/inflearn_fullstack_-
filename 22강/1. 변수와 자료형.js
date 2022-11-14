32c2ebd0bd481a52b4aec8777fd8a4e6fa40d916
woogi25 <hyunwook1891@gmail.com>
2022-11-14 15:57:08 +0900

    22강 복습
    자료형에 대해서

---
+++ 22강/1. 변수와 자료형.js
@@ -0,0 +1,31 @@
+// 숫자형 자료형 선언
+// var 재선언 O, 재할당 O, (let 재할당O, const 재할당X) = 재선언X
+var num = 1;
+
+var num = 10;
+
+console.log(num);
+
+let num2 = 2;
+
+num2 = 20;
+
+console.log(num2);
+
+
+const num3 = 3;
+// num3 = 30;
+
+console.log(num3);
+
+//문자형 자료형 선언
+let string = "안녕"
+
+console.log(string);
+
+let bool = true;
+
+let bool2 = 100 > 0;
+
+console.log(bool2);
+
